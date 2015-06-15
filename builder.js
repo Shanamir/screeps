92f4156917d1b41083db3931ff4e8e4308ca907f
Shanamir <shinamiry@yandex.ru>
2015-06-16 00:41:38 +0600

    строитель
    + модуль строителя

---
+++ builder.js
@@ -0,0 +1,13 @@
+module.exports = function (creep) {
+    if(creep.energy === 0) {
+		creep.moveTo(Game.spawns.Spawn1);
+		Game.spawns.Spawn1.transferEnergy(creep);
+	}
+	else {
+		var target = creep.pos.findNearest(Game.CONSTRUCTION_SITES);
+		if(target) {
+			creep.moveTo(target);
+			creep.build(target);
+		}
+	}
+  }
