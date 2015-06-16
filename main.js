75a821c24ededa8bb1ed5f10aad6ab2693196de2
Shanamir <shinamiry@yandex.ru>
2015-06-16 16:36:34 +0600

    Правки в строительстве
    + Добавлена глобальная функция поиска и сохранения ближайшего объекта для строительства
    * Исправлено определение количества необходимых строителей

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+Creep.prototype.findConstructionSite = function()
+{
+	var target = this.pos.findClosest(FIND_CONSTRUCTION_SITES);
+	this.memory.target = target.id;
+}
+
 var totalHarvesters = 0;
 var totalBuilders = 0;
+var neededBuilers = 1;
 
 var harvester = require('harvester');
 var builder = require('builder');
@@ -26,28 +33,25 @@ for (var i in Memory.roads)
 {
     if (Memory.roads[i] > 3)
     {
-		//console.log(Memory.roadsid[i].roomName);
-		Game.rooms[Memory.roadsid[i].roomName].createConstructionSite(Memory.roadsid[i].x, Memory.roadsid[i].y, STRUCTURE_ROAD);
+	Game.rooms[Memory.roadsid[i].roomName].createConstructionSite(Memory.roadsid[i].x, Memory.roadsid[i].y, STRUCTURE_ROAD);
         delete Memory.roads[i];
     }
 }
 
 while(totalHarvesters < 4) 
 {
-		Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE], '' , { role: 'harvester'});
-		totalHarvesters++;
+	Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE], '' , { role: 'harvester'});
+	totalHarvesters++;
 }
 
 var construction = Game.spawns.Spawn1.room.find(FIND_CONSTRUCTION_SITES, {filter: {my : true}});
-
-var neededBuilers = 1;
 
 if (totalBuilders != 0 && construction.length / totalBuilders >= 10)
 {
     neededBuilers = Math.floor(construction.length / 10);
 }
 
-if (neededBuilers < 1 && construction.length > 0)
+if (totalBuilders < neededBuilers && construction.length > 0)
 {
 	Game.spawns.Spawn1.createCreep([WORK, WORK, CARRY, MOVE], '', { role: 'builder'});
 }
