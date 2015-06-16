b44494490e354cb2b1371c852eadc7f0464faf99
Shanamir <shinamiry@yandex.ru>
2015-06-16 16:37:31 +0600

    Запоминание цели
    * Теперь строитель запоминает цель, над которой работает, и строит её до конца. Используется глобальная функция-прототип

--- builder.js
+++ builder.js
@@ -1,14 +1,32 @@
 module.exports = function (creep) {
-    if(creep.energy === 0) {
+    if(creep.energy === 0) 
+	{
 		creep.moveTo(Game.spawns.Spawn1);
 		Game.spawns.Spawn1.transferEnergy(creep);
 	}
-	else {
-		var target = creep.pos.findClosest(FIND_CONSTRUCTION_SITES);
+	else 
+	{
+		var target = undefined;
+		creep.say(creep.memory.target);
+		if (creep.memory.target === undefined)
+		{
+			creep.findConstructionSite();
+		}
+		else
+		{
+			var targets  = creep.room.find(FIND_CONSTRUCTION_SITES, {filter: {id:creep.memory.target}});
+			if (targets.length > 0)
+				target = targets[0];
+			else
+			{
+				creep.findConstructionSite();
+			}
+		}
+	
 		if(target) 
 		{
 			creep.moveTo(target);
 			creep.build(target);
-		}
+		}	
 	}
   }
