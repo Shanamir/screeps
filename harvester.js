3308a33392eab5e59f3bbaba16a817f307f08781
Shanamir <shinamiry@yandex.ru>
2015-06-16 13:39:08 +0600

    Улучшен поиск источников харвестерами
    * Теперь харвестеры ищут ближайший источник энергии, а не первый в списке
    * Теперь харвестеры сдают энергию в ближайший спавн, а не в Spawn1

--- harvester.js
+++ harvester.js
@@ -2,14 +2,14 @@ module.exports = function (creep)
 {
 	if(creep.energy < creep.energyCapacity)
 	{
-		var sources = creep.room.find(FIND_SOURCES);
-		creep.moveTo(sources[0]);
-		creep.harvest(sources[0]);
-		//Memory.roads.push(creep.pos);
+		var source = creep.pos.findClosest(FIND_SOURCES);
+		creep.moveTo(source);
+		creep.harvest(source);
 	}
 	else 
 	{
-		creep.moveTo(Game.spawns.Spawn1);
-		creep.transferEnergy(Game.spawns.Spawn1)
+		var spawn = creep.pos.findClosest(FIND_MY_SPAWNS);
+		creep.moveTo(spawn);
+		creep.transferEnergy(spawn);
 	}
 }
